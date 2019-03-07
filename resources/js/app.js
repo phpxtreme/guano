c145b5ca48cf17a0d2715616555e2c777077f654
Leonardo Yanes Batista <leo.phpxtreme@gmail.com>
2019-03-07 12:14:48 -0500

    First Commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
-
 /**
- * First, we will load all of this project's Javascript utilities and other
- * dependencies. Then, we will be ready to develop a robust and powerful
- * application frontend using useful Laravel and JavaScript libraries.
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
  */
 
 require('./bootstrap');
